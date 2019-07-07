a48d39e4af64d01c967ca7a7edd7eaf793dc6336
szenadam <szenadam1@gmail.com>
2019-07-07 17:15:44 +0200

    Rename folders

---
+++ Sorting/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Sorting.Sorting
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var foo = new E1(10);
+            var bar = new E1(20);
+            var baz = new E1(30);
+
+            foo.next = bar;
+            bar.next = baz;
+
+            Utils.Util.WriteS1L(foo);
+
+            return 0;
+        }
+    }
+}
