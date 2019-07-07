2afc946011c51a69edcdf2f580ee66eb23959fbd
szenadam <szenadam1@gmail.com>
2019-07-07 17:38:11 +0200

    Create Quicksort test, WIP

---
+++ Sorting.Test/QuicksortTest.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace Sorting.Test
+{
+    public class QuicksortTest
+    {
+        private readonly int[] _array = {5, 4, 3, 2, 1};
+
+        [Fact]
+        public void Should_Sort_Array()
+        {
+            Quicksort.Qs(_array);
+            Assert.Equal(1, _array[0]);
+        }
+    }
+}
